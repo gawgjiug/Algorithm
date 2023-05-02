35818e8b882db934073439904a611d2c26781c4e
gwagjiug <kwjo0228@naver.com>
2023-05-03 00:49:03 +0900

    commit

---
+++ src/Longlong.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Longlong {
+
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String str = "";
+
+        for(int i=1;i<=n/4;i++){
+            str += "long ";
+        }
+        System.out.println(str + "int");
+
+
+
+
+
+
+
+
+
+    }
+}
