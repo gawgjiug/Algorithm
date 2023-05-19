b873a39072063220128cc536db6f9b09524345c1
gwagjiug <kwjo0228@naver.com>
2023-05-19 20:28:16 +0900

    백준 11654

---
+++ src/String_4.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class String_4 {
+    public static void main(String[] args) throws IOException {
+
+        int a = System.in.read();
+        System.out.print(a);
+        //System.in 은 byte 값으로 문자 한 개만 읽으면서 해당 문자에 대응되는 아스키코드 값을 저장할 수 있다
+    }
+}
